251646220a6bf662bb051b8ffa2f2a087b0ef30f
Blob Garry <blobgarry211@gmail.com>
2025-10-24 16:52:26 +0700

    Assertion test.py

---
+++ assertion test.py
@@ -0,0 +1,17 @@
+import unittest
+from string_utils import reverse_string, capitalize_string, is_capitalized 
+
+class TestStringUtils(unittest.TestCase):
+
+    def test_reverse_string(self):
+        self.assertEqual(reverse_string('mochi', 'ichom'))
+
+    def test_capitalize_string(self):
+        self.assertEqual(capitalize_string('sultan', 'Sultan'))
+    
+    def test_is_capitalized(self):
+        self.assertTrue(is_capitalized('Cow'))
+        self.assertFalse(is_capitalized('cow'))
+
+if __name__ == '__main__':
+    unittest.main()
